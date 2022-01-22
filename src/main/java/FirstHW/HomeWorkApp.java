6d4951a2171f6f5890b5f66eead3b255b1bb42f9
Victoria <viktoriya.zyk@mail.ru>
2022-01-22 21:21:26 +0300

    method compareNumbers

--- src/main/java/FirstHW/HomeWorkApp.java
+++ src/main/java/FirstHW/HomeWorkApp.java
@@ -5,7 +5,7 @@ public class HomeWorkApp {
         printThreeWords();
         checkSumSign();
         printColor();
-
+        compareNumbers();
     }
 
     public static void printThreeWords() {
@@ -28,12 +28,22 @@ public class HomeWorkApp {
 
         if (value <= 0) {
             System.out.println("Красный");
-        } else if ( value > 0 && value <= 100) {
+        } else if (value > 0 && value <= 100) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
         }
     }
 
+    public static void compareNumbers() {
+        int a = 9;
+        int b = 12;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
 }
 

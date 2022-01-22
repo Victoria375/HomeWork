679df962e94f33cc1fc9ebe37b5edf247dccabd2
Victoria <viktoriya.zyk@mail.ru>
2022-01-22 20:47:12 +0300

    method printThreeWords()

--- src/main/java/FirstHW/HomeWorkApp.java
+++ src/main/java/FirstHW/HomeWorkApp.java
@@ -2,7 +2,12 @@ package FirstHW;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        
+        printThreeWords();
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange\n" + "Banana\n" + "Apple");
     }
 }
 

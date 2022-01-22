e3b78afcbf92613ab6d35bff9f3c99f70c1d1d59
Victoria <viktoriya.zyk@mail.ru>
2022-01-22 20:58:04 +0300

    method checkSumSign

--- src/main/java/FirstHW/HomeWorkApp.java
+++ src/main/java/FirstHW/HomeWorkApp.java
@@ -3,11 +3,21 @@ package FirstHW;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
 
     }
 
     public static void printThreeWords() {
         System.out.println("Orange\n" + "Banana\n" + "Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = -6;
+        int b = 3;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else System.out.println("Сумма отрицательная");
     }
 }
 

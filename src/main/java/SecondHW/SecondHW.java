38361f66fe8e77e5cc3b9ff919ecf413fcd46de5
Victoria <viktoriya.zyk@mail.ru>
2022-01-25 21:07:32 +0300

    methods numbers10and20 and positiveOrNegative

---
+++ src/main/java/SecondHW/SecondHW.java
@@ -0,0 +1,43 @@
+package SecondHW;
+
+public class SecondHW {
+    public static void main(String[] args) {
+        System.out.println(numbers10and20(20, 5));
+        positiveOrNegative(-1);
+
+
+    }
+
+    /* 1. Написать метод, принимающий на вход два целых числа и проверяющий,
+    что их сумма лежит в пределах от 10 до 20 (включительно),
+    если да – вернуть true, в противном случае – false. */
+
+    public static boolean numbers10and20(int a, int b) {
+        if (a + b >= 10 && a + b <= 20) {
+            return true;
+        }
+        return false;
+    }
+
+    /* 2. Написать метод, которому в качестве параметра передается целое число,
+    метод должен напечатать в консоль, положительное ли число передали или отрицательное.
+    Замечание: ноль считаем положительным числом. */
+
+    public static void positiveOrNegative(int a) {
+        if (a >= 0) {
+            System.out.println("Число " + a + " положительное.");
+        } else {
+            System.out.println("Число " + a + " отрицательное.");
+        }
+    }
+
+    /* 3. Написать метод, которому в качестве параметра передается целое число.
+    Метод должен вернуть true, если число отрицательное, и вернуть false
+    если положительное. */
+
+    public static boolean negative(int a) {
+
+    }
+
+
+}

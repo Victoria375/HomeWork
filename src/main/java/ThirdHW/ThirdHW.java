b789a3db264e766589d75aa3b7e7fa9f39f4e3f8
Victoria <viktoriya.zyk@mail.ru>
2022-01-29 17:20:46 +0300

    task3

--- src/main/java/ThirdHW/ThirdHW.java
+++ src/main/java/ThirdHW/ThirdHW.java
@@ -4,8 +4,8 @@ public class ThirdHW {
     public static void main(String[] args) {
 
         /* 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
-    Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия
-    заменить 0 на 1, 1 на 0 */
+        Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия
+        заменить 0 на 1, 1 на 0 */
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         print(arr);
         System.out.println();
@@ -19,7 +19,7 @@ public class ThirdHW {
         System.out.println("\n");
 
         /* 2. Задать пустой целочисленный массив длиной 100. С помощью цикла
-    заполнить его значениями 1 2 3 4 5 6 7 8 … 100 */
+        заполнить его значениями 1 2 3 4 5 6 7 8 … 100 */
         int[] array = new int[100];
         print(array);
         int num = 1;
@@ -32,32 +32,27 @@ public class ThirdHW {
 
         System.out.println("\n");
 
+        /* 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти
+        по нему циклом, и числа меньшие 6 умножить на 2 */
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        print(arr3);
+        for (int i = 0; i < arr3.length; i++) {
+            if (arr3[i] < 6) {
+                arr3[i] = arr3[i] * 2;
+            }
+        }
+        System.out.println();
+        print(arr3);
+
+        System.out.println("\n");
+
 
 
     }
-
-
-
-
-
-
 
     public static void print(int[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }

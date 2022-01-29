0c2264c2876b5eb5d2b156732bb5b08c7acc8261
Victoria <viktoriya.zyk@mail.ru>
2022-01-29 17:12:43 +0300

    task2

--- src/main/java/ThirdHW/ThirdHW.java
+++ src/main/java/ThirdHW/ThirdHW.java
@@ -2,6 +2,7 @@ package ThirdHW;
 
 public class ThirdHW {
     public static void main(String[] args) {
+
         /* 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
     Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия
     заменить 0 на 1, 1 на 0 */
@@ -14,7 +15,30 @@ public class ThirdHW {
             } else arr[i] = 0;
         }
         print(arr);
+
+        System.out.println("\n");
+
+        /* 2. Задать пустой целочисленный массив длиной 100. С помощью цикла
+    заполнить его значениями 1 2 3 4 5 6 7 8 … 100 */
+        int[] array = new int[100];
+        print(array);
+        int num = 1;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = num;
+            num++;
+        }
+        System.out.println();
+        print(array);
+
+        System.out.println("\n");
+
+
+
     }
+
+
+
+
 
 
 
